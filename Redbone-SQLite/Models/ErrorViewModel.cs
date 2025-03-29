aedc36cdcd292c44261e3e339b53c574cd706dd6
Kate Lin <juni070127@gmail.com>
2025-03-29 13:02:23 +0900

    cdnjs로 변경

--- Redbone-SQLite/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Redbone.Models;
-
-public class ErrorViewModel
-{
-    public string RequestId { get; init; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
--- Redbone-SQLite/wwwroot/assets/js/post.js
+++
@@ -1,7 +0,0 @@
-﻿//Disqus 댓글창을 활성화하기 위한 코드
-(function() {
-    var d = document, s = d.createElement('script');
-    s.src = 'https://katelin.disqus.com/embed.js';
-    s.setAttribute('data-timestamp', +new Date());
-    (d.head || d.body).appendChild(s);
-})();
